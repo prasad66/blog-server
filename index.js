7dd2790a46861996c4b3fb01876dbc60fedf3898
prasad66 <ndpk.prasad@gmail.com>
2022-04-18 10:07:16 +0530

    core update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require('multer');
 const app = express();
+const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoute = require('./routes/auth');
 const usersRoute = require('./routes/users');
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 app.use(express.json());
+app.use(cors());
 app.use('/images', express.static(path.join(__dirname, '/images')));
 
 mongoose.connect(process.env.MONGO_URL, {
